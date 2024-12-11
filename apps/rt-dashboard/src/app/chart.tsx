80ec71a8604879e77cf232a20cf0a4ddf5f17406
dongwei <2458183101@qq.com>
2024-12-11 10:35:37 +0800

    fix: charts1

--- apps/rt-dashboard/src/app/chart.tsx
+++ apps/rt-dashboard/src/app/chart.tsx
@@ -1,27 +1,23 @@
 import { useTranslation } from 'react-i18next';
 import { Image, Dropdown, MenuProps, Space } from 'antd';
-import { useEffect, useState } from 'react';
+import { useMemo, useState } from 'react';
 import { CaretDownOutlined, InteractionOutlined } from '@ant-design/icons';
 import { LibAxios, RenderProps } from '@mf-td/lib-axios';
 import * as echarts from 'echarts';
-export type Main1Props = {
+export type MainProps = {
   activeKey: number;
   coinData: any[];
 };
-export function Charts(props: Main1Props) {
-  const { t } = useTranslation('dashboard');
+export function Charts(props: Readonly<MainProps>) {
+  const { t } = useTranslation('statistic-analysis');
   const { activeKey, coinData } = props;
-  console.log(coinData[activeKey],"coinData");
-
   const [nowTmsp, setNowTmsp] = useState<number>(
     new Date(new Date().toLocaleDateString('en-US')).getTime()
   );
-  useEffect(()=>{
-    // setStablecoinCode(props.coinData[activeKey].code)
-  },[props])
-  const [stablecoinCode, setStablecoinCode] = useState<string>('');
   const [interval, setIntervals] = useState<number>(nowTmsp - 7 * 86400000);
-  const [selectDateText, setSelectDateText] = useState(t('dashboard_0007'));
+  const [selectDateText, setSelectDateText] = useState(
+    t('statistic_analysis_0008')
+  );
   const activeDateList = [
     {
       label: t('statistic_analysis_0008'),
@@ -40,79 +36,134 @@ export function Charts(props: Main1Props) {
     }
   ];
   const items: MenuProps['items'] = activeDateList;
-  const handleClick: MenuProps['onClick'] = ({ key }) => {
-    const fData = activeDateList.filter((item) => item.key === key);
-    setSelectDateText(fData[0].label);
-    const now = new Date(new Date().toLocaleDateString('en-US')).getTime();
-    setNowTmsp(now);
-    setIntervals(now - fData[0].timeStamp);
-  };
+  const showText = useMemo(() => {
+   if (props.coinData.length > 0) {
+      return (
+        <LibAxios
+          key={activeKey}
+          url="/api/manage/v1/td/dashboard/wallet/statistics"
+          method="POST"
+          data={{
+            stablecoinCode: coinData[activeKey].code,
+            startTime: String(interval),
+            endTime: String(nowTmsp)
+          }}
+          onSuccess={(data) => {
+            console.log(data,"data");
 
+            const myChart = echarts.init(
+              document.getElementById('main1') as HTMLElement
+            );
+            myChart.clear();
+            myChart.setOption({
+              tooltip: {
+                trigger: 'axis'
+              },
+              legend: {
+                data: [t('statistic_analysis_0013'), t('statistic_analysis_0017')]
+              },
+
+              calculable: true,
+              xAxis: [
+                {
+                  type: 'category',
+                  data: data.map((item: any) => {
+                    return 'awawd';
+                  })
+                }
+              ],
+              yAxis: [
+                {
+                  type: 'value'
+                }
+              ],
+              series: [
+                {
+                  name: t('statistic_analysis_0016'),
+                  type: 'bar',
+                  data: data.map((item: any) => item.walletNumber)
+                },
+                {
+                  name: t('statistic_analysis_0017'),
+                  type: 'bar',
+                  data: data.map((item: any) => item.walletNewNumber)
+                }
+              ]
+            });
+          }}
+        >
+          {({ data, refetch }: RenderProps) => {
+            return (
+              <div className="bg-white shadow-lg my-6">
+                <div className="w-full flex justify-between">
+                  <div className="bg-white shadow-lg rounded-xl p-4 w-full">
+                    <div className="w-full flex justify-between">
+                      <div className="flex items-center">
+                        <span className="text-base font-extrabold">
+                          {t('statistic_analysis_0011')}
+                        </span>
+                        <InteractionOutlined
+                          className="w-8 ml-4 cursor-pointer"
+                          onClick={() => refetch()}
+                        />
+                      </div>
+                      <div className="flex items-center">
+                        <Image
+                          preview={false}
+                          src="/stablecoin/images/day.svg"
+                          alt=""
+                          width={'1rem'}
+                          height={'1rem'}
+                        />
+                        <Dropdown
+                          menu={{
+                            items,
+                            onClick: ({ key }) => {
+                              const fData = activeDateList.filter(
+                                (item) => item.key === key
+                              );
+                              setSelectDateText(fData[0].label);
+                              const now = new Date(
+                                new Date().toLocaleDateString('en-US')
+                              ).getTime();
+                              setNowTmsp(now);
+                              setIntervals(now - fData[0].timeStamp);
+                              refetch();
+                            }
+                          }}
+                          className="px-2"
+                        >
+                          <a onClick={(e) => e.preventDefault()}>
+                            <Space>
+                              <span className="text-fontTheme cursor-pointer text-base font-extrabold">
+                                {selectDateText}
+                              </span>
+                            </Space>
+                          </a>
+                        </Dropdown>
+
+                        <CaretDownOutlined className="text-fontTheme" />
+                      </div>
+                    </div>
+                    <div
+                      id="main1"
+                      className="pl-12 mt-10"
+                      style={{
+                        width: 'auto',
+                        height: 500
+                      }}
+                    ></div>
+                  </div>
+                </div>
+              </div>
+            );
+          }}
+        </LibAxios>
+      );
+    }
+  }, [activeKey, coinData]);
   return (
-    <div className="bg-white shadow-lg my-6">
-      <div className="w-full flex justify-between">
-        <div className="bg-white shadow-lg rounded-xl p-4 w-full">
-          <div className="w-full flex justify-between">
-            <div className="flex items-center">
-              <span className="text-base font-extrabold">
-                {t('dashboard_0010')}
-              </span>
-            </div>
-            <div className="flex items-center">
-              <Image
-                preview={false}
-                src="/stablecoin/images/day.svg"
-                alt=""
-                width={'1rem'}
-                height={'1rem'}
-              />
-              <Dropdown menu={{ items, onClick: handleClick }} className="px-2">
-                <a onClick={(e) => e.preventDefault()}>
-                  <Space>
-                    <span className="text-fontTheme cursor-pointer text-base font-extrabold">
-                      {selectDateText}
-                    </span>
-                  </Space>
-                </a>
-              </Dropdown>
-
-              <CaretDownOutlined className="text-fontTheme" />
-            </div>
-          </div>
-          <LibAxios
-            url="/api/manage/v1/td/dashboard/wallet/statistics"
-            method="POST"
-            data={{
-              stablecoinCode: "e461250256cd4c569b04617ca08913f0",
-              startTime: String(interval),
-              endTime: String(nowTmsp)
-            }}
-            onSuccess={(data, response) => {
-              console.log(data, 'data');
-            }}
-          >
-            {({ data, refetch }: RenderProps) => {
-              return (
-                data && (
-                  <InteractionOutlined
-                    className="w-8 ml-4 cursor-pointer"
-                    onClick={() => refetch()}
-                  />
-                )
-              );
-            }}
-          </LibAxios>
-          <div
-            id="main"
-            className="pl-12 mt-10"
-            style={{
-              width: 'auto',
-              height: 500
-            }}
-          ></div>
-        </div>
-      </div>
-    </div>
+    <div>{showText}</div>
   );
 }
 
